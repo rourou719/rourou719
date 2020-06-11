eb4780497c3226d1ab2f7a8c55bf6b3f017d7047
rourou719 <869357594@qq.com>
2020-06-11 14:42:31 +0800

    Site updated: 2020-06-11 14:42:29

--- dist/music.js
+++ dist/music.js
@@ -6,13 +6,13 @@ const ap = new APlayer({
       {
         name: "Whiskey and Morphine",
         artist: 'Alexander Jean',
-        url: 'http://music.163.com/song?id=416700932&userid=79118658',
+        url: 'http://music.163.com/song/media/outer/url?id=416700932.mp3',
         cover: 'http://p2.music.126.net/QqFe_pFAsOlrmW1UN4gLuw==/109951163509974538.jpg',
       },
       {
         name: 'I Found Myself',
         artist: 'ZEFEAR',
-        url: 'http://music.163.com/song?id=1450782389&userid=79118658',
+        url: 'http://music.163.com/song/media/outer/url?id=1450782389.mp3',
         cover:'http://p3.music.126.net/tBTNafgjNnTL1KlZMt7lVA==/18885211718935735.jpg',
       },
    
