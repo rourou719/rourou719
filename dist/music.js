0fa9f219f2a43e9a098555666456f0f35c5f77b5
rourou719 <869357594@qq.com>
2020-06-11 15:21:30 +0800

    Site updated: 2020-06-11 15:21:29

--- dist/music.js
+++ dist/music.js
@@ -1,33 +1,28 @@
-<script>
-const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    mini: true,            <!-- 不启用迷你播放模式 -->
-    autoplay: true,        <!-- 音频不自动播放 -->
-    theme: '#FADFA3',       <!-- 主题色 -->
-    loop: 'all',            <!-- 音频循环播放, 可选值: 'all', 'one', 'none'-->
-    order: 'random',        <!-- 音频循环顺序, 可选值: 'list', 'random'-->
-    preload: 'auto',        <!-- 预加载，可选值: 'none', 'metadata', 'auto'-->
-    volume: 1,            <!-- 默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效-->
-    mutex: true,            <!-- 互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器 -->
-    listFolded: false,      <!-- 列表默认折叠， false表示不折叠-->
-    listMaxHeight: '120px', <!-- 列表最大高度，超出此高度将有滚动条-->
-    lrcType: 0,             <!-- 歌词类型，0表示不显示歌词-->
-    audio: [
+{% aplayerlist %}
+{
+    "narrow": false,                          // （可选）播放器袖珍风格
+    "autoplay": true,                         // （可选) 自动播放，移动端浏览器暂时不支持此功能
+    "mode": "random",                         // （可选）曲目循环类型，有 'random'（随机播放）, 'single' (单曲播放), 'circulation' (循环播放), 'order' (列表播放)， 默认：'circulation' 
+    "showlrc": 3,                             // （可选）歌词显示配置项，可选项有：1,2,3
+    "mutex": true,                            // （可选）该选项开启时，如果同页面有其他 aplayer 播放，该播放器会暂停
+    "theme": "#e6d0b2",	                      // （可选）播放器风格色彩设置，默认：#b7daff
+    "preload": "metadata",                    // （可选）音乐文件预载入模式，可选项： 'none' 'metadata' 'auto', 默认: 'auto'
+    "listmaxheight": "513px",                 // (可选) 该播放列表的最大长度
+    "music": [
         {
             name: 'Whiskey and Morphine',                         <!-- 歌曲名称-->
             artist: 'Alexander Jean',                       <!-- 歌曲作者-->
             url: 'http://music.163.com/song/media/outer/url?id=416700932.mp3',     <!-- 歌曲路径-->
             cover: 'http://p3.music.126.net/tBTNafgjNnTL1KlZMt7lVA==/18885211718935735.jpg', <!-- 歌曲封面图片-->
-            theme: '#46718b'                       <!-- 选中歌曲的主题-->
+         
         },
         {
             name: 'I Found Myself',
             artist: 'ZEFEAR,Teya Flow',
             url: 'http://music.163.com/song/media/outer/url?id=1450782389.mp3',
             cover: 'http://p3.music.126.net/tBTNafgjNnTL1KlZMt7lVA==/18885211718935735.jpg',
-            theme: '#46718b'
+    
         },
-   
     ]
-});
-</script>
+}
+{% endaplayerlist %}
